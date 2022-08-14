8e30ac7ea3547ede915f440610e7370b851b8141
Anonymous <hq7d1p4o@pm.me>
2022-08-15 00:18:31 +0800

    chore: update build-macos.sh

--- build-macos.sh
+++ build-macos.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-APP="Example.app"
+APP="WaltTools.app"
 mkdir -p $APP/Contents/{MacOS,Resources}
-go build -o $APP/Contents/MacOS/lorca-example
+go build -o $APP/Contents/MacOS/json-tools
 cat > $APP/Contents/Info.plist << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
 	<key>CFBundleExecutable</key>
-	<string>lorca-example</string>
+	<string>json-tools</string>
 	<key>CFBundleIconFile</key>
 	<string>icon.icns</string>
 	<key>CFBundleIdentifier</key>
-	<string>com.zserge.lorca.example</string>
+	<string>xyz.waltdev.tools.json</string>
 </dict>
 </plist>
 EOF
