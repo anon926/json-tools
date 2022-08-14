bb85c6f862c1c028530d6fd05855438c89330193
Anonymous <hq7d1p4o@pm.me>
2022-08-14 18:38:52 +0800

    feat: json parser ui

---
+++ ui/src/components/JsonParser.js
@@ -0,0 +1,118 @@
+import {Dialog, Switch, Transition} from '@headlessui/react'
+import ReactJson from 'react-json-view'
+import {Fragment, useState} from 'react'
+
+export default function JsonParser() {
+    const [isEditorOpen, setIsEditorOpen] = useState(false)
+    const [enableRecursive, setEnableRecursive] = useState(false)
+    const [editorText, setEditorText] = useState("")
+    const [jsonText, setJsonText] = useState("")
+    const [jsonObject, setJsonObject] = useState({})
+
+    function closeEditor() {
+        setEditorText(jsonText)
+        setIsEditorOpen(false)
+    }
+
+    function saveEditor() {
+        setJsonText(editorText)
+        setJsonObject(JSON.parse(editorText))
+        setIsEditorOpen(false)
+    }
+
+    function openEditor() {
+        setEditorText(jsonText)
+        setIsEditorOpen(true)
+    }
+
+    return (<div className="flex-1 flex flex-col">
+        <div className="flex p-2 justify-start">
+            <button
+                type="button"
+                onClick={openEditor}
+                className="rounded-md bg-sky-500 bg-opacity-80 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+            >
+                Edit JSON
+            </button>
+
+            <Switch.Group>
+                <div className="flex items-center mx-3">
+                    <Switch
+                        checked={enableRecursive}
+                        onChange={setEnableRecursive}
+                        className={`${enableRecursive ? 'bg-emerald-500' : 'bg-gray-200'} relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+                    >
+                          <span
+                              className={`${enableRecursive ? 'translate-x-6' : 'translate-x-1'} inline-block h-4 w-4 transform rounded-full bg-white transition-transform`}
+                          />
+                    </Switch>
+                    <Switch.Label className="ml-3">Parse all strings</Switch.Label>
+                </div>
+            </Switch.Group>
+        </div>
+        <div className="flex-1 rounded-xl m-2 p-4 bg-blue-100 text-left">
+            <ReactJson src={jsonObject}/>
+        </div>
+        <Transition appear show={isEditorOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={() => {
+            }}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25"/>
+                </Transition.Child>
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex flex-col min-h-full items-center justify-center p-6 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel
+                                className="w-full flex-1 flex flex-col transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                <Dialog.Title
+                                    as="h3"
+                                    className="text-lg font-medium leading-6 text-gray-900"
+                                >
+                                    Please enter a JSON string
+                                </Dialog.Title>
+                                <div className="mt-2 flex-1 flex flex-col">
+                                        <textarea
+                                            className="flex-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                                            value={editorText}
+                                            onChange={e => setEditorText(e.target.value)}
+                                        ></textarea>
+                                </div>
+
+                                <div className="mt-4 flex justify-end">
+                                    <button
+                                        type="button"
+                                        className="inline-flex justify-center rounded-md border border-transparent bg-rose-100 px-4 py-2 text-sm font-medium text-rose-900 hover:bg-rose-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-rose-500 focus-visible:ring-offset-2"
+                                        onClick={closeEditor}
+                                    >Cancel
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="ml-6 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                        onClick={saveEditor}
+                                    >Save
+                                    </button>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    </div>)
+}
--- ui/src/components/Tabs.js
+++ ui/src/components/Tabs.js
@@ -1,65 +1,56 @@
 import {Tab} from '@headlessui/react'
+import {CodeIcon, EyeIcon, QuestionMarkCircleIcon} from '@heroicons/react/solid'
 import {classNames} from '../utils/common'
+import JsonParser from './JsonParser'
 
 const tabList = [
     {
         name: "JSON Parser",
-        component: () => (
-            <div>JSON Parser</div>
-        )
+        title: <div className="w-full">
+            <CodeIcon className="h-4 w-4 inline" aria-hidden="true"/>
+            <span className="ml-2">JSON Parser</span>
+        </div>,
+        component: <JsonParser></JsonParser>
     },
     {
         name: "Dag Viewer",
-        component: () => (
-            <div>Dag Viewer</div>
-        )
+        title: <div className="w-full">
+            <EyeIcon className="h-4 w-4 inline" aria-hidden="true"/>
+            <span className="ml-2">Dag Viewer</span>
+        </div>,
+        component: <div>Dag Viewer</div>
     },
     {
         name: "Help",
-        component: () => (
-            <div>Help</div>
-        )
+        title: <div className="w-full">
+            <QuestionMarkCircleIcon className="h-4 w-4 inline" aria-hidden="true"/>
+            <span className="ml-2">Help</span>
+        </div>,
+        component: <div>If you have any questions, please contact wangweichen.walt</div>
     }
 ]
 
 export default function Tabs() {
-    return (
-        <div className="min-h-full overflow-hidden">
-            <div className="container mx-auto px-4 py-5">
-                <Tab.Group>
-                    <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-                        {tabList.map((t, idx) => (
-                            <Tab
-                                key={idx}
-                                className={({selected}) =>
-                                    classNames(
-                                        'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
-                                        'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                                        selected
-                                            ? 'bg-white shadow'
-                                            : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
-                                    )
-                                }
-                            >
-                                {t.name}
-                            </Tab>
-                        ))}
-                    </Tab.List>
-                    <Tab.Panels className="mt-3">
-                        {tabList.map((t, idx) => (
-                            <Tab.Panel
-                                key={idx}
-                                className={classNames(
-                                    'rounded-xl bg-white p-3 min-h-[200px]',
-                                    'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
-                                )}
-                            >
-                                {t.component}
-                            </Tab.Panel>
-                        ))}
-                    </Tab.Panels>
-                </Tab.Group>
-            </div>
+    return (<div className="min-h-full overflow-hidden flex flex-col">
+        <div className="container mx-auto px-4 py-5 flex-1 flex flex-col">
+            <Tab.Group>
+                <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+                    {tabList.map((t, idx) => (<Tab
+                        key={idx}
+                        className={({selected}) => classNames('w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700', 'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2', selected ? 'bg-white shadow' : 'text-blue-100 hover:bg-white/[0.12] hover:text-white')}
+                    >
+                        {t.title}
+                    </Tab>))}
+                </Tab.List>
+                <Tab.Panels className="mt-3 flex-1 flex flex-col">
+                    {tabList.map((t, idx) => (<Tab.Panel
+                        key={idx}
+                        className={classNames('rounded-xl bg-white p-3 flex-1 flex flex-col', 'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2')}
+                    >
+                        {t.component}
+                    </Tab.Panel>))}
+                </Tab.Panels>
+            </Tab.Group>
         </div>
-    )
+    </div>)
 }
