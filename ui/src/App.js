ddec7a40d5818bf4435f4915a07ee40188969cc4
Anonymous <hq7d1p4o@pm.me>
2022-08-21 22:35:00 +0800

    chore: add recoil

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
+import './App.css'
 import Tabs from './components/Tabs'
 
-function App() {
-    return (
-        <div className="App h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-            <Tabs></Tabs>
-        </div>
-    );
+function App () {
+  return (
+    <div className="App h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+      <Tabs></Tabs>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- ui/src/utils/common.js
+++ ui/src/utils/common.js
@@ -1,3 +1,3 @@
 export const classNames = function (...classes) {
-    return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ')
 }
